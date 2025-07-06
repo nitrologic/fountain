5ef4caeff3be93a638378efb26c7425eb3772aa3
simon m2 <nitrologic@gmail.com>
2025-07-06 14:10:25 +1200

    bump macos test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 pushd roha
 export USERNAME="$USER"
 export USERDOMAIN=$(scutil --get LocalHostName)
-deno run --allow-net --allow-run --allow-env --allow-read --allow-write=./forge fountain.js
+deno task roha
 popd
