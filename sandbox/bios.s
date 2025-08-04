2e7a9bc10dc016f158e085a5299c6075199157f8
simon pi5m2 <nitrologic@gmail.com>
2025-08-04 06:37:50 +0100

    mips rom cpp

--- sandbox/bios.s
+++ sandbox/bios.s
@@ -1,15 +1,12 @@
-	section 	coolshit
+.section .text,"ax"
+.global SYS_RESET
 
-	global	SYS_RESET
-
-SYS_RESET:	
-    
-    lui	gp,0x1f80
-	lui	sp,0x8020
-	addiu	sp,sp,-36
-	add	s8,zero,zero
-
-	lw	ra,0(sp)
-	addiu	sp,sp,4
-	jr	ra
+SYS_RESET:
+	lui	$gp,0x1f80
+	lui	$sp,0x8020
+	addiu	$sp,$sp,-36
+	add	$s8,$zero,$zero
+	lw	$ra,0($sp)
+	addiu	$sp,$sp,4
+	jr	$ra
 	nop
--- sandbox/build.sh
+++ sandbox/build.sh
@@ -1,4 +1,17 @@
-export PATH=$PATH:~/x-tools/mipsel-unknown-elf/bin
-mipsel-unknown-elf-gcc --version
-mipsel-unknown-elf-gcc -v -march=mips1 -msoft-float -nostdlib -Ttext=0x80010000 -o test.elf test.c
-mipsel-unknown-elf-readelf -h test.elf
+export PATH=$PATH:~/x-tools/mipsel-elf/bin
+
+#mipsel-elf-as --version
+
+mipsel-elf-as -o bios.o bios.s
+
+#mipsel-elf-gcc --version
+#mipsel-elf-gcc -v -march=mips1 -msoft-float -nostdlib -Ttext=0x80010000 -o test.elf test.c
+
+#mipsel-elf-gcc -x c++ -march=mips1 -msoft-float -nostdlib -c -o test.o test.cpp
+mipsel-elf-g++ -march=mips1 -msoft-float -nostdlib -c -o mipsrom.o mipsrom.cpp
+
+#mipsel-elf-g++ --version
+
+mipsel-elf-gcc -march=mips1 -msoft-float -nostdlib -Ttext=0x80010000 -o mips1.elf bios.o mipsrom.o
+
+mipsel-elf-readelf -h mips1.elf
