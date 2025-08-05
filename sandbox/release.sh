01127a6c6ee07b6cf1ed8bad591acf4c7643e0c3
simon m2 <nitrologic@gmail.com>
2025-08-05 18:49:32 +1200

    mips2.elf 3

--- sandbox/release.sh
+++ sandbox/release.sh
@@ -3,4 +3,6 @@ mipsel-elf-as -msoft-float -o bios.o bios.s
 mipsel-elf-g++ -O2 -msoft-float -nostdlib -c -o mipsrom.o mipsrom.cpp
 mipsel-elf-gcc -O2 -s -msoft-float -nostdlib -Ttext=0x80010000 -o mips2.elf bios.o mipsrom.o
 mipsel-elf-readelf -h mips2.elf
-mipsel-elf-nm mips2.elf
+mipsel-elf-nm --size-sort mips2.elf
+mipsel-elf-size mips2.elf
+mipsel-elf-objdump -h mips2.elf
