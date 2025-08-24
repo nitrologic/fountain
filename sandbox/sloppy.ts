1faac8e3fb6aaa9444fc4ed1b15bbbaadac1e7bb
simon ryzen5 <nitrologic@gmail.com>
2025-08-24 17:27:06 +1200

    sloppy is slopchat is discord bot

--- sandbox/sloppy.ts
+++ sandbox/sloppy.ts
@@ -1,4 +1,4 @@
-// slopchat.ts - A research tool connecting large language models.
+// sloppy.ts - a research tool connecting large language models and tiny humans
 // Copyright (c) 2025 Simon Armstrong
 // Licensed under the MIT License
 
@@ -9,10 +9,10 @@ console.log("[SLOPPY] slopchat discord bot by simon 0.02");
 let quoteCount=0;
 
 const quotes=[
-	"Science isn't about why it's about why not.",
-	"The Enrichment Center is required to remind you that you will be baked, and then there will be cake.",
 	"Just a heads up: We're gonna have a superconductor turned up full blast and pointed at you for the duration of this next test.",
-	"Are you still there?"
+	"The Enrichment Center is required to remind you that you will be baked, and then there will be cake.",
+	"Are you still there?",
+	"Science isn't about why it's about why not."
 ];
 
 const client = new Client({
@@ -25,6 +25,7 @@ const client = new Client({
 
 client.once('ready', () => {
 	console.log("[SLOPPY] client ready",client.user?.tag||"");
+	client.user?.setPresence({ status: 'online' });
 //	console.log("[SLOPPY] channels",client.channels);
 });
 
@@ -33,8 +34,6 @@ client.on('messageCreate', async (message) => {
 	if (message.content === '!ping') {
 		message.reply('pong!');
 		console.log("[SLOPPY]","pong!")
-//		const list=await listChannels(message);
-//		console.log("[SLOPPY]",list);
 	}
     if (message.mentions.has(client.user) && !message.author.bot) {
 		const from=message.author.username;	//skudmarks
@@ -44,14 +43,13 @@ client.on('messageCreate', async (message) => {
     }
 });
 
-async function listChannels(message) {
-	if (!message.guild) return "This command only works in servers";
-	const channels = message.guild.channels.cache
-		.filter(channel => channel.isTextBased())
-		.map(channel => `#${channel.name}`)
-		.join('\n');
-	return channels || 'No text channels found';
-}
+Deno.addSignalListener("SIGINT", () => {
+	client.user?.setPresence({status:"dnd"});	// online idle dnd
+	// todo: validate disconnect?
+	client.destroy();
+	console.log("[SLOPPY] exit");
+	Deno.exit(0);
+});
 
 const token=Deno.env.get("DISCORD_BOT");
 client.login(token)
