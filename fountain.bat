867b4001c7a3ce1d219c3b811970e9810a2ca5af
simon ryzen5 <nitrologic@gmail.com>
2025-10-15 08:10:16 +1300

    revert prompt

--- fountain.bat
+++ fountain.bat
@@ -1,13 +1,8 @@
-chcp 65001
 echo off
+echo %LANG%
+chcp 65001 > nul
 echo Testing Slop Fountain 
 echo see roha/deno.json for task options
-rem 
-rem fountain skeleton play net host 
 pushd roha
 deno task fountain
-rem deno task play
-rem deno task net
-rem deno task host
-rem deno task hello
 popd
