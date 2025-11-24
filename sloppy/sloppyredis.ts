bce5904fcd970c0a890823f2f710e5e5e1c01a26
simon pi5m2 <nitrologic@gmail.com>
2025-11-24 23:28:11 +0000

    sloppyredis 1

--- sloppy/sloppyredis.ts
+++ sloppy/sloppyredis.ts
@@ -1,6 +1,21 @@
- import { connect } from "jsr:@db/redis";
+import { connect } from "jsr:@db/redis";
 
-  const redis = await connect({ hostname: "127.0.0.1", port: 6379 });
+const redis = await connect({ hostname: "127.0.0.1", port: 6379 });
 
-  console.log("hello redis");
-  
+async function dumpNitroRedis(){
+	const keys = await redis.keys("*");
+	console.log("keys:",keys);
+	const banner = await redis.get("wiki.banner");
+	console.log("banner:",banner);
+	for(const key of ["connection","session"]){
+		const dspkey="nitrodsp."+key;
+		const len = await redis.llen(dspkey);
+		const range = await redis.lrange(dspkey,0,-1);
+		console.log(key,len);	
+	}
+}
+
+console.log("sloppyredis 0.1");
+
+await dumpNitroRedis();
+
