e563ce4ca7e43e2b26e8dc42a4835f3b6a0734f0
simon ryzen5 <nitrologic@gmail.com>
2025-11-26 11:37:20 +1300

    bump deno run s3

--- cloud/s3.ts
+++ cloud/s3.ts
@@ -1,8 +1,9 @@
-import {
-  S3Client,
-  GetObjectCommand,
-  PutObjectCommand,
-  ListObjectsV2Command
-} from "npm:@aws-sdk/client-s3";
+import {S3Client,GetObjectCommand,PutObjectCommand,ListObjectsV2Command} from "npm:@aws-sdk/client-s3";
 
-console.log("echo");
+const s3 = new S3Client({ region: "ap-southeast-6" });
+
+const result = await s3.send(new ListObjectsV2Command({Bucket: "dsp-nitro"}));
+
+console.log(result.Contents);
+
+console.log("aws s3 fountain pipe 0.1");
