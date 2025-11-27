08b62b95a286e7833c5567b240a6c0d61e41a31f
simon m2 <nitrologic@gmail.com>
2025-11-27 20:54:24 +1300

    bump

--- sloppy/sloppypipe.ts
+++ sloppy/sloppypipe.ts
@@ -11,7 +11,7 @@ import {wrapText,onSystem,readSystem,readFountain,writeFountain,disconnectFounta
 const sockPath="/tmp/sloppy.sock";
 
 const brandSloppyPipe="SloppyPipe";
-const sloppypipeVersion="1.0.0";
+const sloppypipeVersion="1.1.0";
 const appDetails=brandSloppyPipe+" "+sloppypipeVersion+" @ "+sockPath;
 
 console.log(appDetails);
