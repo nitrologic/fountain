8d881503ea00dde9f7ece5919127c9e747f7452c
simon ryzen5 <nitrologic@gmail.com>
2025-12-03 11:52:10 +1300

    blend 0.01

---
+++ lab/blenderscan/blend.ts
@@ -0,0 +1,19 @@
+// blend.ts - builds gltf libraries from blender files
+// Copyright (c) 2025 Simon Armstrong
+// All rights reserved
+
+import { walk } from "https://deno.land/std/fs/mod.ts";
+import { join, basename } from "https://deno.land/std/path/mod.ts";
+
+const path="C:\\nitrologic\\dsptool\\cbmpico\\models\\commodore-64-computer-full-pack\\source";
+
+let count=0;
+console.log("blend 0.01");
+
+for await (const entry of walk(path, { exts: [".blend"] })) {
+
+    console.log(entry.path);
+    count++;
+}
+
+console.log("complete collected:",count)
