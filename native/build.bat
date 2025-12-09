af1b7d46d946943a075ef96b7b1970043be4bd6d
simon ryzen5 <nitrologic@gmail.com>
2025-12-10 06:36:50 +1300

    native 2

--- native/build.bat
+++ native/build.bat
@@ -1,8 +1,21 @@
+@echo off
+
+rem clean target bin
 rmdir /s /q bin
 mkdir bin
-cmake -G "Visual Studio 17 2022" -S . -B bin
-pushd bin
-rem ninja make whu
-cmake --build . --config Release
-popd
 
+rem generate solution
+rem   -S .   : source directory is current folder
+rem   -B bin : put all generated files into .\bin
+rem cmake -G "Visual Studio 17 2022" -S . -B bin
+cmake -S . -B bin
+
+rem enter build folder
+pushd bin
+
+rem compile the project (Release config)
+rem   --build .        : build inside current directory
+rem   --config Release : use Release configuration
+cmake --build . --config Release
+
+popd
